996a1bab7cdbd72295ee181700a908d8d88c4f11
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-10-31 17:34:07 -0300

    add project Paletas de Cores

---
+++ 7-paleta-de-cores/script.js
@@ -0,0 +1,30 @@
+const button = document.querySelector("button")
+const body = document.body
+const colorsDiv = document.querySelector('.colors')
+
+function createFiveColors() {
+  colorsDiv.innerHTML = ""
+  for(let i = 0; i < 5; i++){     
+    const colorCreated = generateRamdomColor()   
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = colorCreated
+    const textColor = document.createElement('p')
+    textColor.textContent = colorCreated
+    textColor.style.color = colorCreated
+    colorDiv.appendChild(textColor)
+    colorsDiv.appendChild(colorDiv)
+    
+  }    
+  colorsDiv.appendChild('')
+} 
+
+function generateRamdomColor() {
+  const leters = "1234567890ABCDEF"
+  let color = "#"
+  for(let i = 0; i < 6; i++ ) {       
+    color += leters[Math.floor(Math.random() * leters.length)]       
+  }
+   return color
+  }
+  
+button.addEventListener('click',  createFiveColors)
