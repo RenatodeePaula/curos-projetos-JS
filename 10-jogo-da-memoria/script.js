3a36c0ec385bac61524face970aae00e78fe007b
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-11-21 11:48:42 -0300

    Jogo de memória primeiro commit

---
+++ 10-jogo-da-memoria/script.js
@@ -0,0 +1,129 @@
+// Crie uma matriz com pares de números representando as cartas
+const cards = [1, 1, 2, 2, 3, 3, 4, 4]
+
+// Crie um objeto para armazenar as imagens correspondentes para cada carta
+async function generateImagePairs() {
+    const imagePairs = {}
+
+    for(let i = 0; i < cards.length; i++ ) {
+        if(!imagePairs[cards[i]]) {
+            const id = Math.floor(Math.random() * 1000) + 1
+            const url = `https://picsum.photos/id/${id}/300/400`
+            imagePairs[cards[i]] = [url, url]           
+        }
+    }    
+    return imagePairs
+}
+
+// Embaralhe a matriz de cartas
+function shuffleCards(cards) {
+    cards.sort(() => Math.random() - 0.5)
+}
+
+
+// Selicione as cartasd e atribua um número da matriz a cada carta
+async function creatCards() {
+    const imagePairs = await generateImagePairs()
+    shuffleCards(cards)
+
+    const cardList = document.querySelector('.container')
+
+    for(let i = 0; i < cards.length; i++) {
+        
+        const card = document.createElement('div')
+        const cardBack = document.createElement('div')
+        const cardFront = document.createElement('div')
+
+        card.classList.add('card')
+        cardBack.classList.add('back')
+        cardFront.classList.add('front')
+
+        cardBack.style.backgroundImage = `url('assets/card-back.png')`
+       
+        const cardNumber = cards[i]
+        const cardImage = imagePairs[cardNumber].pop()
+        cardFront.style.backgroundImage = ` url(${cardImage})`
+
+        card.setAttribute('data-card', cardNumber)
+        card.appendChild(cardBack)
+        card.appendChild(cardFront)
+
+        card.addEventListener("click", flipCard)
+        cardList.appendChild(card)
+
+    }
+}
+// Variáveis para armazenar o número d cartas viradas, a primeiro qeu foi clicada, a segunda que foi clicada e número de tentativas.
+
+let flippedCards = 0
+let firstCard, secondCard
+let attempts = 0
+
+ // Vire as carta clicada
+function flipCard() {
+    if(flippedCards < 2 && !this.classList.contains('flip')) {
+        flippedCards++        
+        this.classList.add('flip')
+        if(flippedCards === 1) {
+            firstCard = this           
+        } else {
+            secondCard = this
+            attempts++
+            uptatedAttempts()
+            checkForMatch()
+        }
+    }
+}
+
+// Verifique se as cartas viradas são iguais
+function checkForMatch() {
+    const isMatch = firstCard.getAttribute("data-card") ===
+     secondCard.getAttribute("data-card")
+     isMatch ? disableCards() : unFlipCards()
+}
+
+// Atualiza o número de tentativas
+function uptatedAttempts() {
+    const attemptsElement = document.querySelector('.attempts')
+    attemptsElement.textContent = `Tentativas: ${attempts}`
+}
+
+
+// Desabilitar carta que deu match (que forem iguais)
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard)
+    secondCard.removeEventListener("click", flipCard)
+
+    document.querySelectorAll("card:not(.flip").length
+    if(document.querySelectorAll(".card:not(.flip)").length === 0){
+        showCongratulations()
+    }
+    resetBoard()
+
+}
+
+// Parabenizar o jogador que conseguiu ganhar o jogo com o número de tentativas
+function showCongratulations() {
+    const congratulationsContainer = document.querySelector('.congratulations-container')
+    const congratulations = document.createElement('p')
+    congratulationsContainer.appendChild(congratulations)
+    congratulationsContainer.classList.add('congratulations')
+    congratulations.textContent = `Parabéns, você completou o jogo com ${attempts} tentativas!`
+}
+
+// Desvira as cartas se não forem iguais
+function unFlipCards() {
+    setTimeout( () => {
+    firstCard.classList.remove('flip')
+     secondCard.classList.remove('flip')
+     resetBoard()
+    }, 1500)
+}
+
+// Reinicia o tabuleiro
+function resetBoard() {
+    [flippedCards, firstCard, secondCard] = [0, null, null]
+}
+
+
+creatCards()
