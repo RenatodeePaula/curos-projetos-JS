dc419733283cf345d0bd76997fcc36126557d050
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-11-22 12:48:11 -0300

    jogo da memória final

--- 10-jogo-da-memoria/script.js
+++ 10-jogo-da-memoria/script.js
@@ -2,14 +2,15 @@
 const cards = [1, 1, 2, 2, 3, 3, 4, 4]
 
 // Crie um objeto para armazenar as imagens correspondentes para cada carta
-async function generateImagePairs() {
+async function generateImagePairs( ) {
     const imagePairs = {}
 
-    for(let i = 0; i < cards.length; i++ ) {
+    for(let i = 0; i < cards.length; i++) {
+
         if(!imagePairs[cards[i]]) {
             const id = Math.floor(Math.random() * 1000) + 1
-            const url = `https://picsum.photos/id/${id}/300/400`
-            imagePairs[cards[i]] = [url, url]           
+            const  url = `https://picsum.photos/id/${id}/200/300`
+            imagePairs[cards[i]] = [url, url]
         }
     }    
     return imagePairs
@@ -17,19 +18,18 @@ async function generateImagePairs() {
 
 // Embaralhe a matriz de cartas
 function shuffleCards(cards) {
-    cards.sort(() => Math.random() - 0.5)
+    cards.sort( () => Math.random() - 0.5)
 }
 
-
-// Selicione as cartasd e atribua um número da matriz a cada carta
-async function creatCards() {
+// Selicione as cartas e atribua um número da matriz a cada carta
+async function createCards() {
     const imagePairs = await generateImagePairs()
     shuffleCards(cards)
-
-    const cardList = document.querySelector('.container')
+    
+    const container = document.querySelector('.container')
 
     for(let i = 0; i < cards.length; i++) {
-        
+
         const card = document.createElement('div')
         const cardBack = document.createElement('div')
         const cardFront = document.createElement('div')
@@ -38,92 +38,90 @@ async function creatCards() {
         cardBack.classList.add('back')
         cardFront.classList.add('front')
 
-        cardBack.style.backgroundImage = `url('assets/card-back.png')`
-       
-        const cardNumber = cards[i]
-        const cardImage = imagePairs[cardNumber].pop()
-        cardFront.style.backgroundImage = ` url(${cardImage})`
+        cardBack.style.backgroundImage = `url('./assets/card-back.png')`;
+        
+        const cardNumber = cards[i];
+        const cardImage = imagePairs[cardNumber].pop();
+        
+        cardFront.style.backgroundImage = `url(${cardImage})`
+        card.setAttribute("data-card", cardNumber)
 
-        card.setAttribute('data-card', cardNumber)
         card.appendChild(cardBack)
         card.appendChild(cardFront)
 
-        card.addEventListener("click", flipCard)
-        cardList.appendChild(card)
+        container.appendChild(card)
 
+        card.addEventListener('click', flipCard) 
     }
 }
-// Variáveis para armazenar o número d cartas viradas, a primeiro qeu foi clicada, a segunda que foi clicada e número de tentativas.
 
-let flippedCards = 0
+createCards()
+
+// Variáveis para armazenar o número d cartas viradas, a primeiro qeu foi clicada, a segunda que foi clicada e número de tentativas.
+let flipedCards = 0
 let firstCard, secondCard
 let attempts = 0
 
  // Vire as carta clicada
-function flipCard() {
-    if(flippedCards < 2 && !this.classList.contains('flip')) {
-        flippedCards++        
-        this.classList.add('flip')
-        if(flippedCards === 1) {
-            firstCard = this           
+ function flipCard() {   
+    if(flipedCards < 2 & !this.classList.contains("flip")) {
+        flipedCards++
+        this.classList.add("flip")
+        if(flipedCards === 1) {
+            firstCard = this
         } else {
             secondCard = this
             attempts++
-            uptatedAttempts()
+            updateAttempts()
             checkForMatch()
-        }
+          }            
+        } 
     }
-}
-
-// Verifique se as cartas viradas são iguais
-function checkForMatch() {
-    const isMatch = firstCard.getAttribute("data-card") ===
-     secondCard.getAttribute("data-card")
-     isMatch ? disableCards() : unFlipCards()
-}
 
 // Atualiza o número de tentativas
-function uptatedAttempts() {
+function updateAttempts() {
     const attemptsElement = document.querySelector('.attempts')
     attemptsElement.textContent = `Tentativas: ${attempts}`
 }
 
-
 // Desabilitar carta que deu match (que forem iguais)
-function disableCards() {
-    firstCard.removeEventListener("click", flipCard)
-    secondCard.removeEventListener("click", flipCard)
+function checkForMatch() {
+    const isMatch = firstCard.getAttribute('data-card') === secondCard.getAttribute('data-card')
+    isMatch ? disabledCards() : unflipCards() 
 
-    document.querySelectorAll("card:not(.flip").length
-    if(document.querySelectorAll(".card:not(.flip)").length === 0){
-        showCongratulations()
-    }
-    resetBoard()
+}
 
+// Desabilidar as cartas se forem iguais
+function disabledCards() {
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+
+    if (document.querySelectorAll(".card:not(.flip)").length === 0) {
+        showCongratulationsMessage()
+      }    
+      resetBoard();
 }
 
 // Parabenizar o jogador que conseguiu ganhar o jogo com o número de tentativas
-function showCongratulations() {
-    const congratulationsContainer = document.querySelector('.congratulations-container')
-    const congratulations = document.createElement('p')
-    congratulationsContainer.appendChild(congratulations)
-    congratulationsContainer.classList.add('congratulations')
-    congratulations.textContent = `Parabéns, você completou o jogo com ${attempts} tentativas!`
+function showCongratulationsMessage() {
+    const congratulationsMessage = document.querySelector('.congratulations-container')
+    const congratulationsPhrase = document.createElement('p')
+    congratulationsPhrase.classList.add('congratulations')
+    congratulationsPhrase.textContent  = `Parabéns você venceu com ${attempts} tentativas!`
+    congratulationsMessage.appendChild(congratulationsPhrase) 
+
 }
 
 // Desvira as cartas se não forem iguais
-function unFlipCards() {
-    setTimeout( () => {
-    firstCard.classList.remove('flip')
-     secondCard.classList.remove('flip')
-     resetBoard()
-    }, 1500)
-}
+function unflipCards() {
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+      resetBoard();
+    }, 1000);
+  }
 
 // Reinicia o tabuleiro
 function resetBoard() {
-    [flippedCards, firstCard, secondCard] = [0, null, null]
+    [firstCard, secondCard, flipedCards] = [null, null, 0]
 }
-
-
-creatCards()
