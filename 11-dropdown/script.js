b82a21223e94358fb305b230d31a36a4aed1dd5c
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-11-24 13:34:37 -0300

    droppdown final

--- 11-dropdown/script.js
+++ 11-dropdown/script.js
@@ -1,11 +1,21 @@
 const backToTopButton = document.querySelector("#back-to-top")
 
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', function()  {
     const scrollTop = document.documentElement.scrollTop
 
-    if(scrollTop > 500) {
-        backToTopButton.addEventListener('click', () => {
+    if(scrollTop > 500) {        
             backToTopButton.style.display = 'block'
-        })
+                
+    } else {    
+        backToTopButton.style.display = "none"
     }
+})
+
+backToTopButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    window.scrollTo({
+        top:0,
+        behavior: "smooth",
+    })
 })
